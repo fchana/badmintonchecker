9f0b753b15059ed5bfa4fa101a33abb3eac77668
it62070148 <54875111+it62070148@users.noreply.github.com>
2019-12-10 10:16:41 +0700

    web
    web v1 test

---
+++ badminton/script.js
@@ -0,0 +1,41 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB2RQo0t2hvMLHVI6CVI0yXYJ4ORwLf_yU",
+  authDomain: "fir-batminton.firebaseapp.com",
+  databaseURL: "https://fir-batminton.firebaseio.com",
+  projectId: "fir-batminton",
+  storageBucket: "fir-batminton.appspot.com",
+  messagingSenderId: "831562283599",
+  appId: "1:831562283599:web:76fb860f6747dc84a0c83a",
+  measurementId: "G-P0QYP4SGZN"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+      var dataref = firebase.database()
+      var count = dataref.ref("/courtall")
+      count.on("value", function(snap){
+      var count = snap.val()
+      var count1 = count.court1
+      console.log(count)
+      if(count1 == "1"){
+          document.getElementById("lightStatus").innerText = "Cort1 is full"
+      }else{
+          document.getElementById("lightStatus").innerText = "Cort1 is empty"
+      }
+      
+      var count2 = count.court2
+      if(count2 == "1"){
+          document.getElementById("lightStatus2").innerText = "Cort2 is full"
+      }else{
+          document.getElementById("lightStatus2").innerText = "Cort2 is empty"
+      }
+      console.log(count2)
+      var count3 = count.court3
+      if(count3 == "1"){
+          document.getElementById("lightStatus3").innerText = "Cort3 is full"
+      }else{
+          document.getElementById("lightStatus3").innerText = "Cort3 is empty"
+      }
+  });
+
