4a5c1b12e2f5ddf7843065371f027f16438118a9
it62070108 <56574626+it62070108@users.noreply.github.com>
2019-12-12 16:32:30 +0700

    Combine LineNotify and Firebase test.
    ทดสอบการรวมกันระหว่าง 2 Libary ว่าสามารถทำงานร่วมกันได้หรือไม่

--- firebase test/Firebasetest.ino
+++ firebase test/Firebasetest.ino
@@ -1,8 +1,10 @@
+#include <TridentTD_LineNotify.h>
 #include <FirebaseArduino.h>
 #define SSID        "oweninwza"
 #define PASSWORD    "0967906611"
 #define FIREBASE_HOST "fir-batminton.firebaseio.com"
 #define FIREBASE_AUTH "PclJoL7BEop8SDgTAbxg01vFdiqsUocdfbrqyh58"
+#define LINE_TOKEN  "IsZu1w8h8rcNKiFIWBX3EpFhwrL2sd7P5XKzjz0bc1G"
 int court1 = 0;
 int court2 = 0;
 int court3 = 0;
@@ -15,9 +17,30 @@ void setup() {
   Serial.printf("\nWiFi connected\nIP : ");
   Serial.println(WiFi.localIP());  
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  LINE.setToken(LINE_TOKEN);
+  LINE.notify("Sever has online");
 }
 
 void loop() {
+  if(court1 == 0){
+    Line.notify("now court1 is empty");
+  else{
+    Line.notify("now court1 is full");
+  }
+  }
+  if(court2 == 0){
+    Line.notify("now court2 is empty");
+  else{
+    Line.notify("now court1 is full");
+  }
+  }
+  if(court3 == 0){
+    Line.notify("now court3 is empty");
+  else{
+    Line.notify("now court1 is full");
+  }
+  }
+
   Firebase.setInt("court1", court1);
   Firebase.setInt("court2", court2);
   Firebase.setInt("court3", court3);
