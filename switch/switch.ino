8a0d0dc48330201eddd0da1db6947744e6e6b0c3
it62070120 <56570607+it62070120@users.noreply.github.com>
2019-12-14 12:25:56 +0700

    Organize code in function format and Change LED out.
    แก้ไขจัดรูปแบบของโค้ดให้อยู่ในรูปแบบของฟังก์ชั่น และ แก้ไข output ของ nodemcu ที่ส่งออกไปยัง switch เพื่อให้ง่ายต่อการออกแบบวงจร

--- switch/switch.ino
+++ switch/switch.ino
@@ -12,6 +12,13 @@ int st = 0;
 int sted = 3;
 
 void setup() {
+  /*
+  In this function,
+   set line token,
+   set firebase host,
+   set input, output
+   and set wifi conection. 
+  */
   pinMode(D0, INPUT);
   pinMode(D1, INPUT);
   pinMode(D2, INPUT);
@@ -31,89 +38,106 @@ void setup() {
 }
 
 void loop() {
-  Firebase.setInt("court1", court1);
-  Firebase.setInt("court2", court2);
-  Firebase.setInt("court3", court3);
-  if(digitalRead(D0) == 0){
-    if(digitalRead(D5) == 0){
+  /*
+  This function is main function of my project
+   that used check court badminton's status.
+  */
+  put2base(court1, court2, court3);
+
+  if(digitalRead(D0) == 0) {
+    if(digitalRead(D5) == 1) {
       court1 = 0;
-      st += 1;
-      digitalWrite(D5, HIGH);
-    }
-  }
-  else{
-    if(digitalRead(D5) == 1){
-      court1 = 1;
       st += 1;
       digitalWrite(D5, LOW);
     }
   }
-  if(digitalRead(D1) == 0){
-    if(digitalRead(D6) == 0){
-      court2 = 0;
+  else{
+    if(digitalRead(D5) == 0) {
+      court1 = 1;
       st += 1;
-      digitalWrite(D6, HIGH);
+      digitalWrite(D5, HIGH);
     }
   }
-  else{
-    if(digitalRead(D6) == 1){
-      court2 = 1;
+  if(digitalRead(D1) == 0) {
+    if(digitalRead(D6) == 1) {
+      court2 = 0;
       st += 1;
       digitalWrite(D6, LOW);
     }
   }
-  if(digitalRead(D2) == 0){
-    if(digitalRead(D7) == 0){
-      court3 = 0;
+  else {
+    if(digitalRead(D6) == 0) {
+      court2 = 1;
       st += 1;
-      digitalWrite(D7, HIGH);
+      digitalWrite(D6, HIGH);
     }
   }
-  else{
-    if(digitalRead(D7) == 1){
-      court3 = 1;
+  if(digitalRead(D2) == 0) {
+    if(digitalRead(D7) == 1) {
+      court3 = 0;
       st += 1;
       digitalWrite(D7, LOW);
     }
   }
-
-  if(st - sted == 1){
-    if(court1 == 0){
-      if(court2 == 0){
-        if(court3 == 0){
-          LINE.notify("All court are empty.");
-        }
-        else{
-          LINE.notify("Court1 and court2 are empty, court3 is full.");
-        } 
-      }
-      else{
-        if(court3 == 0){
-          LINE.notify("Court1 and court3 are empty, court2 is full.");
-        }
-        else{
-          LINE.notify("Court1 is empty, court3 and court2 are full.");
-        }
-      }
+  else {
+    if(digitalRead(D7) == 0) {
+      court3 = 1;
+      st += 1;
+      digitalWrite(D7, HIGH);
     }
-    else{
-      if(court2 == 0){
-        if(court3 == 0){
-          LINE.notify("Court2 and court3 are empty, court1 is full.");
-        }
-        else{
-          LINE.notify("Court2 is empty, court1 and court3 are full.");
-        }
-      }
-      else{
-        if(court3 == 0){
-          LINE.notify("Court3 is empty, court1 and court2 are full.");
-        }
-        else{
-          LINE.notify("All court are full.");
-        }
-      }
-    }
+  }
+  if(st - sted == 1) {
+    showtext(st, sted, court1, court2, court3);
     sted += 1;
+  }
+}
+
+void put2base(int court1, int court2, int court3) {
+  Firebase.setInt("court1", court1);
+  Firebase.setInt("court2", court2);
+  Firebase.setInt("court3", court3);  
+}
+
+void showtext(int st, int sted, int court1, int court2, int court3) {
+  /*
+  In this function,
+   2 send Line's notification
+   about status of court badminton.
+  */
+  if(court1 == 0) {
+    if(court2 == 0) {
+      if(court3 == 0) {
+        LINE.notify("All court are empty.");
+      }
+      else {
+        LINE.notify("Court1 and court2 are empty, court3 is full.");
+      } 
+    }
+    else {
+      if(court3 == 0) {
+        LINE.notify("Court1 and court3 are empty, court2 is full.");
+      }
+      else {
+        LINE.notify("Court1 is empty, court3 and court2 are full.");
+      }
+    }
+  }
+  else {
+    if(court2 == 0) {
+      if(court3 == 0) {
+        LINE.notify("Court2 and court3 are empty, court1 is full.");
+      }
+      else {
+        LINE.notify("Court2 is empty, court1 and court3 are full.");
+      }
+    }
+    else {
+      if(court3 == 0) {
+        LINE.notify("Court3 is empty, court1 and court2 are full.");
+      }
+      else {
+        LINE.notify("All court are full.");
+      }
+    }
   }
 }
